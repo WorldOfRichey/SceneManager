80295b21443d0fdb9c5593ab440830e15c261491
RICHEYSLAPTOP\Richard <worldofrichey@outlook.com>
2026-03-15 17:28:47 +0000

    made load defered

--- addons/game_state_manager/scripts/scene_loader.gd
+++ addons/game_state_manager/scripts/scene_loader.gd
@@ -7,10 +7,13 @@ func delete_scene_node() -> void:
 	if current_scene != null:
 		current_scene.queue_free()
 
-func load_scene_quick(scene_name : String) -> void :
+func load_scene_quick(path : String) -> void :
+	load_scene_quick_defered.call_deferred(path)
+	
+func load_scene_quick_defered(path : String) -> void :
 	if current_scene != null:
 		current_scene.queue_free()
 		
-	var s = ResourceLoader.load(scene_name)
+	var s = ResourceLoader.load(path)
 	current_scene = s.instantiate()
 	GameManager.game_management.current_scene_node.add_child(current_scene)
