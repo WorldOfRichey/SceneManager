10606a56b638444390ec34e0f8caa506f1c6cd5b
RICHEYSLAPTOP\Richard <worldofrichey@outlook.com>
2026-03-16 07:13:43 +0000

    Added states

--- addons/game_state_manager/scripts/game_manager.gd
+++ addons/game_state_manager/scripts/game_manager.gd
@@ -5,5 +5,21 @@ extends Node
 @onready var transitions_node : Node = get_node("/root/GameManagment/Transitions")
 @onready var game_manager_settings : GameManagerSettings = get_node("/root/GameManagment")
 
+signal signal_on_game_manager_state_changed
+
+enum GameManagerStates { PLAYING, PAUSED, LOADING }
+
+var current_state : GameManagerStates = GameManagerStates.PLAYING
+
+func change_game_manager_state(new_state : GameManagerStates) -> void :
+	#If the new state is the old state do nothing.
+	if current_state == new_state:
+		return
+		
+	#Change the state and emit a signal
+	var old_state = current_state
+	current_state = new_state
+	signal_on_game_manager_state_changed.emit(old_state, current_state)
+
 func get_transitions_node(node_name : String) -> Node :
 	return transitions_node.get_node(node_name)
