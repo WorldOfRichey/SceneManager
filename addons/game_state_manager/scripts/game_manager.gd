ef2c3c91bdd7acac35723f99af6cf85fd16601a1
RICHEYSLAPTOP\Richard <worldofrichey@outlook.com>
2026-03-16 06:54:50 +0000

    refactored names

--- addons/game_state_manager/scripts/game_manager.gd
+++ addons/game_state_manager/scripts/game_manager.gd
@@ -1,3 +1,9 @@
 extends Node
 
-@onready var game_management : GameManagement = get_node("/root/GameManagement")
+@onready var scene_loader : SceneLoader = get_node("/root/GameManagment/SceneLoader")
+@onready var current_scene_node : Node = get_node("/root/GameManagment/CurrentScene")
+@onready var transitions_node : Node = get_node("/root/GameManagment/Transitions")
+@onready var game_manager_settings : GameManagerSettings = get_node("/root/GameManagment")
+
+func get_transitions_node(node_name : String) -> Node :
+	return transitions_node.get_node(node_name)
---
+++ addons/game_state_manager/scripts/game_manager_settings.gd
@@ -0,0 +1,4 @@
+class_name GameManagerSettings
+extends Node
+
+@export_file_path("*.tscn") var initial_scene : String = ""
